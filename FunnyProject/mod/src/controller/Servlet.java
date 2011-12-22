eaea1b75e04beee7b9fed39dc93d93107ab6e9ef
Aisylu <maisylu@mail.ru>
2011-12-22 14:34:26 +0400

    add new folders web, libraries, exception

--- FunnyProject/mod/src/controller/Servlet.java
+++ FunnyProject/mod/src/controller/Servlet.java
@@ -1,11 +1,29 @@
-package controller;
+package Controller;
+
+
+import org.json.simple.JSONArray;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by IntelliJ IDEA.
- * model.vo.User: Aisylu
+ * User: Aisylu
  * Date: 20.12.11
  * Time: 8:59
  * To change this template use File | Settings | File Templates.
  */
-public class Servlet {
+public class Servlet extends HttpServlet{
+
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyy HH:mm:ss");
+
+
 }
